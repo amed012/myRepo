1d9e2f869410388a6d3e90a09dcfa38f40ec61f9
unknown <a.elbaz012@gmail.com>
2022-03-01 11:21:09 -0800

    initialisation de projet

---
+++ src/main/java/com/app/evrestaurant/EvRestaurantApplication.java
@@ -0,0 +1,13 @@
+package com.app.evrestaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvRestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvRestaurantApplication.class, args);
+	}
+
+}
